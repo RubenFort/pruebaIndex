d8d97ef30ae4c2b2bf47906d4104bcaabfa24f9a
Ruben Dario Fortunez Garea <EXTrdfortunez@oesia.com>
2021-04-05 22:44:27 +0200

    subo Variables.java en la carpeta prueba

---
+++ prueba/Variables.java
@@ -0,0 +1,19 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		//Declaro variables
+		int entero =  10;
+		Double doble = 22.5;
+		char letra = 'd';
+		String frase = "Hola mundo";
+		
+		//Imprimo variables por pantalla
+		System.out.println("Valor del int: " + entero);
+		System.out.println("Valor del Double: " + doble);
+		System.out.println("Valor del char: " + letra);
+		System.out.println("Valor del String: " + frase);
+		
+	}
+}
